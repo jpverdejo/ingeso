29ecdf9adfe11cbec32151b562ac4f30d242c2d6
Juan Pablo Verdejo <jpverdejo@groupon.com>
2013-12-05 18:24:14 -0300

    Finishing?

--- app/models/alumno.rb
+++ app/models/alumno.rb
@@ -3,5 +3,5 @@ class Alumno < ActiveRecord::Base
 	has_many :pruebas, :through => :inscripcions
 	has_many :salas, :through => :inscripcions
 	
-	has_attached_file :foto, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :foto, :styles => { :normal => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
