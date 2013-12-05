48b4f3545f001e2b386a19e46f248f888a29f4a4
Juan Pablo Verdejo <jpverdejo@groupon.com>
2013-12-05 10:28:14 -0300

    tas

--- Gemfile
+++ Gemfile
@@ -28,13 +28,7 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
-group :doc do
-  # bundle exec rake doc:rails generates the API under doc/api.
-  gem 'sdoc', require: false
-end
-
-gem 'anjlab-bootstrap-rails', :require => 'bootstrap-rails',
-                              :github => 'anjlab/bootstrap-rails'
+gem 'anjlab-bootstrap-rails', require: 'bootstrap-rails', github: 'anjlab/bootstrap-rails'
 
 gem 'rails_12factor', group: :production
 
@@ -45,7 +39,8 @@ gem 'rails_12factor', group: :production
 # gem 'unicorn'
 
 # Use Capistrano for deployment
-gem 'capistrano', group: :development
+gem 'capistrano', '~> 2.15.5', group: :development
+gem 'rvm-capistrano', group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,26 @@
-set :application, 'ingeso'
-set :repo_url, 'git@github.com:jpverdejo/ingeso.git'
+require 'bundler/capistrano'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :application, "Ingeso"
 
-set :deploy_to, '/home/rails'
-# set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
-
-end
+set :user, 'deploy'
+set :domain, 'deploy@162.243.251.125'
+set :applicationdir, "/home/rails"
+ 
+set :scm, 'git'
+set :repository,  "git@github.com:jpverdejo/ingeso.git"
+#set :git_enable_submodules, 1 # if you have vendored rails
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, false
+ 
+# roles (servers)
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+ 
+# deploy config
+set :deploy_to, applicationdir
+set :deploy_via, :export
+ 
+# additional settings
+default_run_options[:pty] = true  # Forgo errors when deploying from windows
