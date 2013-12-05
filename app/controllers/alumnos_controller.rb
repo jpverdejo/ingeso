2ab0b82b00278850d9724d9cf071e34ad18d486e
Juan Pablo Verdejo <jpverdejo@groupon.com>
2013-12-05 16:07:26 -0300

    Bug fixes

--- app/controllers/alumnos_controller.rb
+++ app/controllers/alumnos_controller.rb
@@ -80,53 +80,63 @@ class AlumnosController < ApplicationController
   end
 
   def doCargar
-    i = 0
+    begin
+      i = 0
 
-    @valid_ruts = []
-    @invalid_ruts = []
-    @updated_ruts = []
+      @valid_ruts = []
+      @invalid_ruts = []
+      @updated_ruts = []
 
-    file_path = params[:csv].path
-    csv = CSV.read(file_path, :encoding => 'windows-1251:utf-8')
-    csv.each do |alumno_csv|
+      file_path = params[:csv].path
+      csv = CSV.read(file_path, :encoding => 'windows-1251:utf-8')
+      csv.each do |alumno_csv|
 
-      if i > 0
-        rut = self.get_rut(alumno_csv[0] + "-" + alumno_csv[1])
-        
-        if rut
-          @valid_ruts << rut
+        if i > 0
+          rut = self.get_rut(alumno_csv[0] + "-" + alumno_csv[1])
+          
+          if rut
+            @valid_ruts << rut
 
-          nombre = alumno_csv[4] + " " + alumno_csv[2] + " " + alumno_csv[3]
-          carrera_id = alumno_csv[5]
-          ingreso_ano = alumno_csv[6]
-          ingreso_semestre = alumno_csv[7]
-          grupo = alumno_csv[12]
-          seccion = alumno_csv[13]
+            nombre = alumno_csv[4] + " " + alumno_csv[2] + " " + alumno_csv[3]
+            carrera_id = alumno_csv[5]
+            ingreso_ano = alumno_csv[6]
+            ingreso_semestre = alumno_csv[7]
+            grupo = alumno_csv[12]
+            seccion = alumno_csv[13]
 
-          alumno = Alumno.find_by_rut(rut)
+            alumno = Alumno.find_by_rut(rut)
 
-          if not alumno
-            alumno = Alumno.new()
+            if not alumno
+              alumno = Alumno.new()
+            else
+              @updated_ruts << rut
+            end
+
+            alumno.rut = rut
+            alumno.nombre = nombre
+            alumno.carrera_id = carrera_id
+            alumno.ingreso_ano = ingreso_ano
+            alumno.ingreso_semestre = ingreso_semestre
+            alumno.grupo = grupo
+            alumno.seccion = seccion
+
+            alumno.save!
+
           else
-            @updated_ruts << rut
+            @invalid_ruts << alumno_csv[0] + "-" + alumno_csv[1]
           end
-
-          alumno.rut = rut
-          alumno.nombre = nombre
-          alumno.carrera_id = carrera_id
-          alumno.ingreso_ano = ingreso_ano
-          alumno.ingreso_semestre = ingreso_semestre
-          alumno.grupo = grupo
-          alumno.seccion = seccion
-
-          alumno.save!
-
-        else
-          @invalid_ruts << alumno_csv[0] + "-" + alumno_csv[1]
         end
-      end
 
-      i += 1
+        i += 1
+      end
+      render
+      return
+    rescue CSV::MalformedCSVError
+      redirect_to action: "index", error: "El archivo no es un CSV valido"
+      return
+    else
+      redirect_to action: "index", error: "Hubo un problema al cargar el CSV"
+      return
     end
   end
 
@@ -135,41 +145,49 @@ class AlumnosController < ApplicationController
   end
 
   def doCargarFotos
-    i = 0
+    begin
+      i = 0
 
-    rand_dir = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
-    extract_dir = "#{Rails.root.to_s}/tmp/extract/#{rand_dir}/"
-    logger.debug extract_dir
-    file_path = params[:zip].path
+      rand_dir = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+      extract_dir = "#{Rails.root.to_s}/tmp/extract/#{rand_dir}/"
+      logger.debug extract_dir
+      file_path = params[:zip].path
 
-    Zip::File.open(file_path) do |zip|
-      zip.each do |file|
-        next if file.name =~ /__MACOSX/ or file.name =~ /\.DS_Store/ or !file.file?
-   
-        f_path=File.join(extract_dir, file.name)
-        FileUtils.mkdir_p(File.dirname(f_path))
-        zip.extract(file, f_path)
+      Zip::File.open(file_path) do |zip|
+        zip.each do |file|
+          next if file.name =~ /__MACOSX/ or file.name =~ /\.DS_Store/ or !file.file?
+     
+          f_path=File.join(extract_dir, file.name)
+          FileUtils.mkdir_p(File.dirname(f_path))
+          zip.extract(file, f_path)
 
-        extension = self.get_image_extension("#{f_path}")
+          extension = self.get_image_extension("#{f_path}")
 
-        if ["png", "jpg", "gif"].include?(extension)
-          rut = file.name.partition('.').first
+          if ["png", "jpg", "gif"].include?(extension)
+            rut = file.name.partition('.').first.partition('/').last
+            alumno = Alumno.find_by_rut(rut)
 
-          alumno = Alumno.find_by_rut(rut)
-
-          if alumno
-            foto = File.open(f_path)
-            alumno.foto = foto
-            foto.close
-            alumno.save!
+            if alumno
+              foto = File.open(f_path)
+              alumno.foto = foto
+              foto.close
+              alumno.save!
+            end
           end
         end
       end
+
+      FileUtils.rm_rf extract_dir
+
+      redirect_to action: "index", notice: "ZIP cargado correctamente"
+      return
+    rescue Zip::ZipError
+      redirect_to action: "index", error: "El archivo no es un ZIP valido"
+      return
+    else 
+      redirect_to action: "index", error: "Hubo un problema al cargar el ZIP"
+      return
     end
-
-    FileUtils.rm_rf extract_dir
-
-    redirect_to action: "index", notice: "ZIP cargado correctamente"
   end
 
   #Method got from: https://github.com/Numerico/rut-chileno/blob/master/lib/rut_chileno.rb
