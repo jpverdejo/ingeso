d15a580101d544721b3e16b6045eba74a3b033da
Juan Pablo Verdejo <jpverdejo@groupon.com>
2013-12-05 12:02:29 -0300

    lo rompi :(

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Ingeso::Application.initialize!
-
-config.to_prepare do
-    Devise::SessionsController.layout proc{ |controller| action_name == 'new' ? "devise"   : "login" }
-end
